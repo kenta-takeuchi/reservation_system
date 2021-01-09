d440b4f85e6bfdd1b5aabcaee022ca59e7dd8677
kenta-takeuchi <49678973+kenta-takeuchi@users.noreply.github.com>
2021-01-09 16:27:21 +0900

    create frontend(react, typescript)

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/auth/', include('djoser.urls')),
+    path('api/v1/auth/', include('djoser.urls.jwt')),
+    path('api/v1/', include('apiv1.urls')),
+    re_path('', RedirectView.as_view(url='/')),
 ]
